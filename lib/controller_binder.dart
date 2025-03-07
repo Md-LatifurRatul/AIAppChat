fad4c4aa299f2f0618be9fc1252908d44d8a8131
Md-LatifurRatul <latifurratulislam@gmail.com>
2025-03-07 21:44:51 +0600

    improve code

--- lib/controller_binder.dart
+++
@@ -1,9 +0,0 @@
-import 'package:aigeminiapp/controller/pick_image_controller.dart';
-import 'package:get/get.dart';
-
-class ControllerBinder extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(PickImageController());
-  }
-}
